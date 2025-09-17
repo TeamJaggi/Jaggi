a253a1cea2afc6aa5500a3f416530cea0ab61919
TeamJaggi <nftjaggi@gmail.com>
2025-09-17 08:29:24 +0530

    Update and rename deepseek_python_20250917_c71784.py to userbot.py

--- userbot.py
+++ userbot.py
@@ -1,21 +1,3 @@
-from flask import Flask
-import threading
-import os
-import time
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "✅ 𝑇𝑟𝑦𝑖𝑛𝑔 𝑇𝑜 𝑇𝑎𝑐𝑘𝑙𝑒 𝑆𝑒𝑡𝑏𝑎𝑐𝑘 𝑇𝐺 - @MrJaggiX!"
-
-def run_flask():
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
-
-# Flask ko background thread me start karo
-threading.Thread(target=run_flask, daemon=True).start()
-
 import os
 from telethon.sessions import StringSession
 from telethon import TelegramClient, events
